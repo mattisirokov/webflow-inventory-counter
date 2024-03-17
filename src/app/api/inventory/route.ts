a94f66e2cd1b32afa6c6c998ec3cf18cbf60ff42
Matti Sirokov <matti@identio.fi>
2024-03-17 12:55:11 +0200

    Remove timestamp param and cache command

--- src/app/api/inventory/route.ts
+++ src/app/api/inventory/route.ts
@@ -3,18 +3,12 @@ export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  // This is to prevent the browser from caching the response, we need to be getting the most recent data from Webflow API....
-  const timestamp = Date.now();
-
   try {
-    const response = await fetch(
-      `${process.env.WEBFLOW_REQUEST_URL}?tld=${timestamp}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.WEBFLOW_BEARER_TOKEN}`,
-        },
-      }
-    );
+    const response = await fetch(`${process.env.WEBFLOW_REQUEST_URL}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.WEBFLOW_BEARER_TOKEN}`,
+      },
+    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -25,7 +19,6 @@ export async function GET(request: Request) {
     return new Response(JSON.stringify(data), {
       headers: {
         "Content-Type": "application/json",
-        "Cache-Control": "no-cache, no-store, must-revalidate",
         "Access-Control-Allow-Origin": "https://gringos.webflow.io",
         "Access-Control-Allow-Methods": "GET",
         "Access-Control-Allow-Headers": "Content-Type",
