765f294cdeefbbcccdda7919c20ed41c9377e2b5
Matti Sirokov <matti@identio.fi>
2024-03-17 12:17:02 +0200

    update CORS policies

--- src/app/api/inventory/route.ts
+++ src/app/api/inventory/route.ts
@@ -26,6 +26,9 @@ export async function GET(request: Request) {
       headers: {
         "Content-Type": "application/json",
         "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Access-Control-Allow-Origin": "https://gringos.webflow.io",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
       },
     });
   } catch (error) {
