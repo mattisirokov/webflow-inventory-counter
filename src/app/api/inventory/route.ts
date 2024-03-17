7704b07ec0142348ce7e70f571c0a4b03d51d684
Matti Sirokov <matti@identio.fi>
2024-03-17 11:28:50 +0200

    init commit with API route

---
+++ src/app/api/inventory/route.ts
@@ -0,0 +1,32 @@
+export const runtime = "nodejs";
+export async function GET(request: Request) {
+  // This is to prevent the browser from caching the response, we need to be getting the most recent data from Webflow API....
+  const timestamp = Date.now();
+
+  try {
+    const response = await fetch(
+      `${process.env.WEBFLOW_REQUEST_URL}?tld=${timestamp}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.WEBFLOW_BEARER_TOKEN}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+      },
+    });
+  } catch (error) {
+    console.error(`Fetch failed: ${error}`);
+    throw error;
+  }
+}
